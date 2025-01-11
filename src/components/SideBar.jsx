fb55c194675d20e299a91c5daf9951ce603aa8d4
Arpit Jana <arpitjana2103@gmail.com>
2025-01-11 13:53:05 +0530

    Adding City-List

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,18 @@
 import style from "../styles/sideBar.module.css";
+import LogoBox from "../components/LogoBox";
+import AppNav from "./AppNav";
+import CityList from "./CityList";
+import Footer from "./Footer";
 
 function SideBar() {
-    return <div className={style.sideBar}>SideBar</div>;
+    return (
+        <div className={style.sideBar}>
+            <LogoBox />
+            <AppNav />
+            <CityList />
+            <Footer />
+        </div>
+    );
 }
 
 export default SideBar;
