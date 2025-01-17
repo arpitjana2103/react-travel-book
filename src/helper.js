0ed610061a54137074fa6826e43deeda7021907a
Arpit Jana <arpitjana2103@gmail.com>
2025-01-17 21:45:34 +0530

    format date form input value

--- src/helper.js
+++ src/helper.js
@@ -5,3 +5,19 @@ export const formatDate = function (date) {
         year: "numeric",
     }).format(new Date(date));
 };
+
+export const formatDateForInputValue = function (arg) {
+    const date = new Date(arg);
+    const mm = date.getMonth() + 1;
+    const dd = date.getDate();
+    const yyyy = date.getFullYear();
+    return `${yyyy}-${mm < 10 ? `0${mm}` : mm}-${dd < 10 ? `0${dd}` : dd}`;
+};
+
+export function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+        .toUpperCase()
+        .split("")
+        .map((char) => 127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
